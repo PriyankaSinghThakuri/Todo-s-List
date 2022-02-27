6700a673d56b452ff557352df79a6eb806e3be82
PriyankaSinghThakuri <Priyankasinghthakuri57@gmail.com>
2022-02-27 13:44:42 +0545

    first commit

---
+++ src/MyComponents/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const About = () => {
+  return (
+        <div>
+      This ia an about Component
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Labore quas perferendis velit,
+        et iusto id est quisquam corrupti, ex inventore perspiciatis, optio provident libero. Repellendus at
+        exercitationem ab praesentium libero!</p>
+        </div>
+  )
+};
